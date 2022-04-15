1fb4560ff9f7f134e2bbfe80316944462e0a794e
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-04-16 00:19:03 +0200

    remaking front

--- login.php
+++ login.php
@@ -13,59 +13,50 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === TRUE) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <title>login</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="css/login.css" />
 </head>
 
 <body>
-    <section class="ftco-section">
-        <div class="container">
-            <div class="row justify-content-center login">
-                <div class="col-md-6 text-center mb-5">
-                    <h2 class="heading-section login-title">
-                        Welcome to ITI Cafeteria
-                    </h2>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-6 col-lg-4 bg-dark py-5" style="border-radius: 15px">
-                    <div class="login-wrap p-0">
-                        <h3 class="mb-4 text-center">Login</h3>
-                        <form action="authenticate.php" method="POST">
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="username" placeholder="Username" value="" required />
-                            </div>
-                            <div class="form-group">
-                                <input id="password-field" type="password" class="form-control" name="password" placeholder="Password" value="" required />
-                                <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="form-control btn btn-primary submit px-3" value="Login">
-                                    Sign In
+    <form action="authenticate.php" method="POST" class="vh-100 gradient-custom">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem">
+                        <div class="card-body p-5 text-center">
+                            <div class="mb-md-5 mt-md-4 pb-5">
+                                <h2 class="mb-md-2 mt-md-2 pb-2">Welcome To ITI Cafeteria</h2>
+                                <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                                <p class="text-white-50 mb-5">
+                                    Please enter your login and password!
+                                </p>
+
+                                <div class="form-outline form-white mb-4">
+                                    <input type="text" name="username" placeholder="Your UserName" value="" required class="form-control form-control-lg" />
+                                    <label class="form-label">User name</label>
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <input type="password" name="password" placeholder="Your Password" value="" required class="form-control form-control-lg" />
+                                    <label class="form-label">Password</label>
+                                </div>
+
+                                <p class="small mb-5 pb-lg-2">
+                                    <a href="forgetPassword.php" class="ForgetPwd text-white-50">Forget Password?</a>
+                                </p>
+                                <button class="btn btn-outline-light btn-lg px-5" type="submit" value="Login">
+                                    Login
                                 </button>
                             </div>
-                            <div class="form-group d-md-flex">
-                                <!-- <div class="w-50">
-                    <label class="checkbox-wrap checkbox-primary"
-                      >Remember Me
-                      <input type="checkbox" checked />
-                      <span class="checkmark"></span>
-                    </label>
-                  </div> -->
-                                <div class="w-50 text-md-right">
-                                    <a href="forgetPassword.php" style="color: #fff">Forgot Password</a>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+    </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
