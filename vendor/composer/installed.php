94576363a6914c3591289c4fcae276618f588b53
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-04-18 16:31:26 +0200

    starting with php mailer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-Abdelrahman-Gbr',
+        'version' => 'dev-Abdelrahman-Gbr',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bd417a1e837fd8812f3b65915d83c25f8bf8840a',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-Abdelrahman-Gbr',
+            'version' => 'dev-Abdelrahman-Gbr',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bd417a1e837fd8812f3b65915d83c25f8bf8840a',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.0',
+            'version' => '6.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'e43bac82edc26ca04b36143a48bde1c051cfd5b1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
