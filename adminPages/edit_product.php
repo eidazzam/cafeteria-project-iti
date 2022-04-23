b406b27ff46649e7b52cd9002459bc7f98c919db
shimaa-00 <shimaakhallaf00@gmail.com>
2022-04-23 21:58:36 +0200

    updating add, update product's style

--- adminPages/edit_product.php
+++ adminPages/edit_product.php
@@ -50,7 +50,7 @@ if (isset($data->file)) {
 <?php
 
 session_start();
-// // If the user is not logged in redirect to the login page...
+// If the user is not logged in navajowhiteirect to the login page...
 // if (!isset($_SESSION['loggedin'])) {
 //     header('Location: ../login.php');
 // }
@@ -65,93 +65,130 @@ session_start();
 <?php
 // include('../includes/header.php'); 
 ?>
-<link rel="stylesheet" href="../css/adminNav.css" />
+<html>
 
-<link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<head>
+    <link rel="stylesheet" href="../css/adminNav.css" />
 
-<main class="container p-4">
-    <div class="row">
-        <div class="col-md-3"></div> <!-- Just Padding  -->
-        <div class="col-md-5">
+    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-            <div class="card card-body">
-                <form action="save_edited_product.php?id=<?php echo "{$_GET['id']}" ?>" method="POST" enctype="multipart/form-data">
 
-                    <div class="title form-group mb-3">
-                        <h1> Update Product </h1>
-                    </div>
+    <style>
+        body {
+            background-image: url("../images/coffe_black.jpg");
+            background-repeat: no-repeat;
+            background-origin: content-box;
+            background-position: center;
+            background-size: cover;
+        }
 
-                    <div class="form-group mb-3">
-                        <input type="text" name="name" class="form-control" placeholder="Product Name" value='<?php echo $product_name; ?>' autofocus>
-                        <p style="color: red;">
-                            <?php
-                            if (isset($errors->name)) {
-                                echo "{$errors->name} ";
-                            }
-                            ?>
-                        </p>
-                    </div>
+        #update {
+            background-color: navajowhite;
+            color: black;
+            font-size: 20px;
+            font-weight: bold;
+        }
 
-                    <div class="form-group mb-3">
-                        <input type="number" name="price" class="form-control" placeholder="cost" min="0" value='<?php echo $product_price; ?>' autofocus>
-                        <p style="color: red;">
-                            <?php
-                            if (isset($errors->price)) {
-                                echo "{$errors->price} ";
-                            }
-                            ?>
-                        </p>
-                    </div>
+        h1 {
+            color: navajowhite;
+            text-align: center;
+        }
 
-                    <div class="form-group mb-3">
-                        <select class="form-control" id="exampleFormControlSelect1" name="category">
+        .card {
+            margin-top: 30px;
+            background-color: black;
+            border-radius: 20px;
+        }
+    </style>
+</head>
 
-                            <?php
-                            $query = "SELECT * FROM category";
-                            $result_tasks = mysqli_query($conn, $query);
-                            while ($row = mysqli_fetch_assoc($result_tasks)) {
-                                echo "<option value='{$row['id']}'>{$row['name']}</option>";
-                            }
-                            ?>
-                        </select>
-                    </div>
+<body>
 
-                    <div class="input-group mb-3">
 
-                        <div class="custom-file">
-                            <input type="file" name="file" class="custom-file-input" aria-describedby="inputGroupFileAddon01" value='<?php echo $product_file ?>'>
-                            <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+    <main class="container p-4">
+        <div class="row">
+            <div class="col-md-4 "></div> <!-- Just Padding  -->
+            <div class="col-md-5">
 
+                <div class="card card-body">
+                    <form action="save_edited_product.php?id=<?php echo "{$_GET['id']}" ?>" method="POST" enctype="multipart/form-data">
+
+                        <div class="title form-group mb-3">
+                            <h1> Update Product </h1>
                         </div>
 
+                        <div class="form-group mb-3">
+                            <input type="text" name="name" class="form-control" placeholder="Product Name" value='<?php echo $product_name; ?>' autofocus>
+                            <p style="color: navajowhite;">
+                                <?php
+                                if (isset($errors->name)) {
+                                    echo "{$errors->name} ";
+                                }
+                                ?>
+                            </p>
+                        </div>
 
-                    </div>
-                    <p style="color: red;" class="mb-3">
+                        <div class="form-group mb-3">
+                            <input type="number" name="price" class="form-control" placeholder="cost" min="0" value='<?php echo $product_price; ?>' autofocus>
+                            <p style="color: navajowhite;">
+                                <?php
+                                if (isset($errors->price)) {
+                                    echo "{$errors->price} ";
+                                }
+                                ?>
+                            </p>
+                        </div>
 
-                        <?php
-                        if (isset($errors->file)) {
-                            echo "{$errors->file} ";
-                        }
-                        ?>
+                        <div class="form-group mb-3">
+                            <select class="form-control" id="exampleFormControlSelect1" name="category">
 
-                    </p>
-                    <input type="submit" name="update_task" class="btn btn-success btn-block mb-3" value="Update Product">
-                </form>
+                                <?php
+                                $query = "SELECT * FROM category";
+                                $result_tasks = mysqli_query($conn, $query);
+                                while ($row = mysqli_fetch_assoc($result_tasks)) {
+                                    echo "<option value='{$row['id']}'>{$row['name']}</option>";
+                                }
+                                ?>
+                            </select>
+                        </div>
 
+                        <div class="input-group mb-3">
+
+                            <div class="custom-file">
+                                <input type="file" name="file" class="custom-file-input" aria-describedby="inputGroupFileAddon01" value='<?php echo $product_file ?>'>
+                                <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+
+                            </div>
+
+
+                        </div>
+                        <p style="color: navajowhite;" class="mb-3">
+
+                            <?php
+                            if (isset($errors->file)) {
+                                echo "{$errors->file} ";
+                            }
+                            ?>
+
+                        </p>
+                        <input type="submit" name="update_task" class="btn  btn-block mb-3" value="Update " id="update">
+                    </form>
+
+                </div>
             </div>
+
         </div>
 
-    </div>
+        </div>
+    </main>
+</body>
 
-    </div>
-</main>
-
-
+</html>
 <?php
 // include('../includes/footer.php'); 
 ?>
