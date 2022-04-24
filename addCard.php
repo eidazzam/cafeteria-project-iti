64c28dd4b3d24df8577aa1f0881741aeb54046dc
Mohamed-Shhata <mohamed01068215253@gmail.com>
2022-04-24 09:17:33 +0200

    home of user and home of admin and payment transaction and store data to database

--- addCard.php
+++ addCard.php
@@ -13,7 +13,25 @@
   
   $cards=explode(" ",$cards);
 ?>
-
+<script>
+  function increasePrice(p, id) {
+    document.getElementById(id).value++;
+    // totalamount += p;
+    
+    document.getElementById("totalPrice").value = parseInt(document.getElementById("totalPrice").value) + parseInt(p);
+  }
+  function decreasePrice(p, id) {
+    if (document.getElementById(id).value > 1) {
+      document.getElementById(id).value--;
+      // if(document.getElementById(id).value > 2)
+      // console.log("object")
+        document.getElementById("totalPrice").value = parseInt(document.getElementById("totalPrice").value) - parseInt(p);
+      // totalamount -= p;
+    }
+    // if (p <= 0) p = 0;
+    // document.getElementById("totalPrice").innerHTML =parseInt(document.getElementById("totalPrice").innerHTML) - parseInt(p);
+  }
+</script>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -33,6 +51,16 @@
     <link rel="stylesheet" href="css/card.css" />
 
     <title>Document</title>
+    <style>
+      input::-webkit-outer-spin-button,
+      input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+      }
+      input[type=number] {
+        -moz-appearance: textfield;
+      }
+    </style>
   </head>
   <body>
     <div class="card">
@@ -48,67 +76,87 @@
               </div>
             </div>
           </div>
-          <?php 
-            $collection=implode(",",$cards);
-            // var_dump($collection);
-           
-              $sum=0;
-              $count=0;
-              $items=$data->select("product","*","product_id in (".substr($collection,0,-1) .")");
-              while($item=$items->fetch_array()){
-              $sum+=$item['price'];
-              $count++;
-          ?>
-          <div class="row border-top border-bottom">
-            <div class="row main align-items-center">
-              <div class="col-2">
-                <img class="img-fluid" src="images/<?php echo $item['pic']?>" />
-              </div>
-              <div class="col">
-                <div class="row text-muted"><?php echo $item['name']?></div>
-                <!-- <div class="row">Cotton T-shirt</div> -->
-              </div>
-              <div class="col">
-                <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a>
-              </div>
-              <div class="col">
-                &euro; <?php echo $item['price']?> 
-                <a href="./removeFromCard.php?items=<?php echo $collection.'&item='.$item['product_id'] ?>">  <span  class="close">&#10005;</span></a>
+          
+            <?php 
+              $collection=implode(",",$cards);
+              // var_dump($collection);
+            
+                $sum=0;
+                $count=0;
+                // $a;
+                $items=$data->select("product","*","product_id in (".substr($collection,0,-1) .")");
+                while($item=$items->fetch_array()){
+                $sum+=$item['price'];
+                $count++;
+                // array_push($a,$item["product_id"]);
+                // $a[$item["product_id"]]=1;
+
+            ?>
+            <form action="orderProcessing.php?id=1" method="post">
+            <div class="row border-top border-bottom">
+              <div class="row main align-items-center">
+                <div class="col-2">
+                  <img class="img-fluid" src="images/<?php echo $item['pic']?>" />
+                </div>
+                <div class="col">
+                  <div class="row text-muted"><?php echo $item['name']?></div>
+                  <!-- <div class="row">Cotton T-shirt</div> -->
+                </div>
+                <div class="col">
+                  <a  onclick=decreasePrice(<?php echo $item['price']. ','. $item["product_id"]?>)>-</a>
+                  
+                    <!-- <input type="number" name="quantity" id="" min="1" style=" width: 35px;"> -->
+                    <input type="number" data-step="1" data-min="0" value="1" id=<?php echo  $item["product_id"] ?> name=<?php echo  $item["product_id"] ?>  title="Qty" class="input-qty qty" size="4" style=" width: 35px;" >                
+                  
+                  <a  onclick=increasePrice(<?php echo $item['price']. ','. $item["product_id"]?>)>+</a>
+                </div>
+                <div class="col">
+                  &euro; <?php echo $item['price']?> 
+                  <a href="./removeFromCard.php?items=<?php echo $collection.'&item='.$item['product_id'] ?>">  <span  class="close">&#10005;</span></a>
+                </div>
               </div>
             </div>
+            <?php  }?>
+
+            <?php
+            // var_dump($a);
+            
+            ?>
+            <div class="back-to-shop">
+              <a href="#">&leftarrow;</a
+              ><span class="text-muted">Back to shop</span>
+            </div>
           </div>
-          <?php  }?>
-          <div class="back-to-shop">
-            <a href="#">&leftarrow;</a
-            ><span class="text-muted">Back to shop</span>
+          <div class="col-md-4 summary">
+            <div>
+              <h5><b>Summary</b></h5>
+            </div>
+            <hr />
+            <div class="row">
+              <div class="col" style="padding-left: 0">ITEMS <?php echo $count ?></div>
+              <div class="col text-right">&euro; <?php echo $sum  ?></div>
+            </div>
+            
+              <p>SHIPPING</p>
+              <select>
+                <option class="text-muted">Standard-Delivery- &euro;5.00</option>
+              </select>
+              <p>GIVE CODE</p>
+              <input id="code" placeholder="Enter your code" />
+            
+            <div
+              class="row"
+              style="border-top: 1px solid rgba(0, 0, 0, 0.1); padding: 2vh 0"
+            >
+              <div class="col">TOTAL PRICE</div>
+              <!-- <div class="col text-right">&euro; <input id="totalPrice" type="number" name="totalPrice" value=<?php echo $sum +5 ?>></div> -->
+              <div class="col text-right">&euro; <input id="totalPrice" type="number" name="totalPrice" value=<?php echo $sum +5 ?>></div>
+
+            </div>
+            <input type="submit" value="CHECKOUT" class="btn" >
+            <!-- <input class="btn"  href=orderProcessing.php?id=<?php echo $_GET['id']?> >CHECKOUT</input> -->
           </div>
-        </div>
-        <div class="col-md-4 summary">
-          <div>
-            <h5><b>Summary</b></h5>
-          </div>
-          <hr />
-          <div class="row">
-            <div class="col" style="padding-left: 0">ITEMS <?php echo $count ?></div>
-            <div class="col text-right">&euro; <?php echo $sum  ?></div>
-          </div>
-          <form>
-            <p>SHIPPING</p>
-            <select>
-              <option class="text-muted">Standard-Delivery- &euro;5.00</option>
-            </select>
-            <p>GIVE CODE</p>
-            <input id="code" placeholder="Enter your code" />
-          </form>
-          <div
-            class="row"
-            style="border-top: 1px solid rgba(0, 0, 0, 0.1); padding: 2vh 0"
-          >
-            <div class="col">TOTAL PRICE</div>
-            <div class="col text-right">&euro; <?php echo $sum +5 ?></div>
-          </div>
-          <button class="btn">CHECKOUT</button>
-        </div>
+        </form>
       </div>
     </div>
 
